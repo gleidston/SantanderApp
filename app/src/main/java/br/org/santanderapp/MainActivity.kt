f5c959f9144174bb794a3fa4a18cc78767c6ca5f
gleidston <king2010-ro@hotmail.com>
2021-07-07 23:45:50 -0300

    terminando o trabalho

---
+++ app/src/main/java/br/org/santanderapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.org.santanderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
